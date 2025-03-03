128e9c4fed815bb1333aeb48b82151da55416b5a
jpingithub <nallabothulanagarjun0@gmail.com>
2025-03-03 12:48:58 +0530

    Made it ready to deploy

--- src/main/java/com/rb/likeandcomment/LikeCommentServiceApplication.java
+++ src/main/java/com/rb/likeandcomment/LikeCommentServiceApplication.java
@@ -2,12 +2,10 @@ package com.rb.likeandcomment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
 public class LikeCommentServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rb/likeandcomment/client/PostClient.java
+++ src/main/java/com/rb/likeandcomment/client/PostClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "POST-SERVICE")
+@FeignClient(name = "POST-SERVICE", url = "${post-service-url}")
 public interface PostClient {
     @GetMapping("/api/v1/posts/{id}")
     ResponseEntity<Post> getPostById(@PathVariable("id") Integer id);
