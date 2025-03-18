34178f0ebbf27e17475968aee2264bc4498f87fe
jpingithub <nallabothulanagarjun0@gmail.com>
2025-03-18 11:14:22 +0530

    Made it working in local

--- src/main/java/com/rb/likeandcomment/client/UserClient.java
+++ src/main/java/com/rb/likeandcomment/client/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "USER-MANAGEMENT-SERVICE", url = "${user-management-url}")
+@FeignClient(name = "USER-MANAGEMENT-SERVICE")
 public interface UserClient {
 
     @GetMapping("/api/v1/users/search")
